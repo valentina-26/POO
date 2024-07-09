f5a44032b54a6599b494a5e9d6090300347a0176
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-07-09 06:43:00 -0500

    feat: :white_check_mark: Ejercicio 6
    Creauna clasellamadaVehiculoconlas siguientespropiedades: marca, modelo y velocidad. La clase debe tener un método llamado acelerar() que incremente la velocidad del vehículo en 10unidades. Crea una instancia de la clase Vehiculo llamada vehiculo1 y asigna valores a sus propiedades. Luego, llama al método acelerar() y muestra la velocidad actual en el HTML. Crea una clase llamada Coche que herede de la claseVehiculo. Agrega una nueva propiedad llamada combustible y redefineel método acelerar() para que incremente la velocidad del coche en20unidades. Crea una instancia de la clase Coche llamada coche1 y asigna valores a sus propiedades. Luego, llama al método acelerar() y muestralavelocidadactualenelHTML. Agrega un método estático a la clase Vehiculo llamado convertirKmHEnMph() que reciba una velocidad en kilómetros por hora y retorne la velocidad equivalente en millas por hora. Para ello, divide la velocidad en kilómetros por hora entre 1.60934. Llama al método estático convertirKmHEnMph() pasando una velocidad en kilómetros por hora y muestra el resultado

--- js/main.js
+++ js/main.js
@@ -1,59 +1,43 @@
-class Figura {
-    constructor(color, area) {
-        this.color = color;
-        this.area = area;
+class Vehiculo {
+    constructor(marca, modelo, velocidad) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
     }
 
-    calcularArea() {
-        console.log(`El area de la figura es ${this.area}`);
+    acelerar() {
+        console.log(this.velocidad * 10);
     }
 
-
+    static convertirKmHEnMph(velocidadKmH) {
+        return velocidadKmH / 1.60934;
+    }
 }
-
 
 document.querySelector('#registrationForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
-    let color = document.querySelector('#color').value;
-    let area = document.querySelector('#area').value;
+    let velocidad = parseFloat(document.querySelector('#velocidad').value); 
+    let marca = document.querySelector('#marca').value;
+    let modelo = document.querySelector('#modelo').value;
 
-    let nuevafigura = new Figura(color, area);
-    nuevafigura.calcularArea();
+    let nuevoVehiculo = new Vehiculo(marca, modelo, velocidad);
+    nuevoVehiculo.acelerar();
+
+    let velocidadMph = Vehiculo.convertirKmHEnMph(velocidad);
+    console.log(`Velocidad en Mph: ${velocidadMph}`);
 });
 
-
-let figura1 = new Figura("Rosa", "24 cm");
-figura1.calcularArea();
-
-
-
-class Circulo extends Figura {
-    constructor(color, area,radio) {
-        super(color, area);
-        this.radio = radio;
+class Coche extends Vehiculo {
+    constructor(velocidad, marca, modelo, combustible) {
+        super(marca, modelo, velocidad);
+        this.combustible = combustible;
     }
 
-    calcularArea() {
-        return Math.PI * this.radio ** 2;
+    acelerar() {
+        console.log(this.velocidad * 20);
     }
 }
 
-
-let circulo1 = new Circulo("Rojo", "10 cm", "5 cm");
-circulo1.calcularArea()
-
-class Rectangulo extends Figura {
-    constructor(color, area,largo, ancho) {
-    super(color,area);
-    this.largo = largo;
-    this.ancho = ancho;
-}
-    calcularArea() {
-        return this.largo * this.ancho;
-}
-}
-
-let recatangulo1 = new Rectangulo("verde","20cm","40cm","20cm");
-recatangulo1.calcularArea()
-
+let coche1 = new Coche(10, "Mazda", "2020", "Gasolina");
+coche1.acelerar();
